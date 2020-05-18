0b731dcce8e74f063051e7a7289a6d855261e438
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2020-05-18 14:54:41 +0900

    Create 큰 놈, 작은 놈, 같은 놈.py

---
+++ D1/큰 놈, 작은 놈, 같은 놈.py
@@ -0,0 +1,10 @@
+T=int(input())
+
+for i in range(T):
+    a,b=map(int, input().split())
+    if a>b:
+        print("#%d >" %(i+1))
+    elif a==b:
+        print("#%d =" %(i+1))
+    else:
+        print("#%d <" %(i+1))
