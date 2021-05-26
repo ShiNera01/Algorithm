b15d39209e1d933e391e7a39aa994c6f6099e8a8
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2021-05-26 20:43:35 +0900

    Create 완주하지 못한 선수.py

---
+++ 프로그래머스_python/해시/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+def solution(participant, completion):
+    answer = ''
+    
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+        else:
+            continue
+                   
+    if answer == '':
+        answer = participant[-1]
+    return answer
