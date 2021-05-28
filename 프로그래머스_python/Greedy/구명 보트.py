1fbb6c22105aa4441211cd8f6b52ccca322199be
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2021-05-28 16:07:02 +0900

    Create 구명 보트.py

---
+++ 프로그래머스_python/Greedy/구명 보트.py
@@ -0,0 +1,23 @@
+def solution(people, limit):
+    answer = 0
+    
+    people.sort(reverse = True)
+    
+    left = 0
+    right = len(people) - 1
+    
+    while left <= right:
+        
+        if left == right:
+            answer += 1
+            break
+        
+        elif people[left] + people[right] > limit:
+            answer += 1
+            left += 1
+        else :
+            answer += 1
+            left += 1
+            right -= 1
+    
+    return answer
