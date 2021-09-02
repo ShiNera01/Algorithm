2882dd01c673c3805758bd48b4238e84156ea3fd
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2021-09-03 00:12:28 +0900

    Create 숫자 문자열과 영단어.py

---
+++ 카카오/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
@@ -0,0 +1,48 @@
+def solution(s):
+    answer = ""
+    
+    word = ""
+    for i in range(len(s)):
+        
+        if s[i].isdigit() == True:
+            answer += s[i]
+        
+        else :
+            word += s[i]
+            if word == "zero":
+                answer += "0"
+                word = ""
+            elif word == "one":
+                answer += "1"
+                word = ""
+            elif word == "two":
+                answer += "2"
+                word = ""
+            elif word == "three":
+                answer += "3"
+                word = ""
+            elif word == "four":
+                answer += "4"
+                word = ""
+            elif word == "five":
+                answer += "5"
+                word = ""
+            elif word == "six":
+                answer += "6"
+                word = ""
+            elif word == "seven":
+                answer += "7"
+                word = ""
+            elif word == "eight":
+                answer += "8"
+                word = ""
+            elif word == "nine":
+                answer += "9"
+                word = ""
+            
+                
+            
+            
+    answer = int(answer)
+    
+    return answer
