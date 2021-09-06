24feb917e777ffa93d10807c0c29b1a86a28d6d6
ShiNera01 <37740450+ShiNera01@users.noreply.github.com>
2021-09-06 23:05:05 +0900

    Update 문자열 압축.py

--- 카카오/2020 카카오 블라인드/문자열 압축.py
+++ 카카오/2020 카카오 블라인드/문자열 압축.py
@@ -1,1 +1,46 @@
 
+def solution(s):
+    answer = len(s)
+
+    
+    
+    
+    
+    
+    for i in range(1, int(len(s)/2) + 1):
+        index = 0
+        word = ""
+        comp_word = ""
+        comp_word = s[index:index+i]
+        
+        count = 0
+        
+        while index + i <= len(s):
+            
+            while index + i <= len(s):
+                if s[index : index + i] == comp_word:
+                    index += i
+                    count += 1
+                else:
+                    break
+                
+            if len(word) == 0 and count == 1:
+                word += comp_word
+            elif len(word) == 0 and count != 1:
+                word += str(count) + comp_word
+            elif count == 1:
+                word = word + comp_word
+            else:
+                word = word + str(count) + comp_word
+            
+            comp_word = s[index:index+i]
+            count = 0
+            
+        
+        if index < len(s):
+            word += s[index:]
+            
+        
+        answer = min(answer,len(word))
+        
+    return answer
